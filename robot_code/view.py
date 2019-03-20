713da3edb591e53e83397ee0921e2294d6e4f6af
Julien Dubois <ptijuju22@gmail.com>
2019-03-20 19:49:10 +0100

    Correcting bugs with new LemAPI GUIs

--- robot_code/view.py
+++ robot_code/view.py
@@ -11,16 +11,22 @@ from lemapi.widget import Button
 class Control_view(View):
     def init_widgets(self):
         w, h = get_gui().get_size()
+        tk = {"textColor": (0, 0, 0, 255)}
         self.add_widget("forward_button", Button, (w * 0.5, h * 0.25), \
-            size=(w * 0.25, h * 0.25), anchor=(0, 0), text="Avancer")
+            size=(w * 0.25, h * 0.25), anchor=(0, 0), text="Avancer", \
+            textKwargs=tk)
         self.add_widget("backward_button", Button, (w * 0.5, h * 0.75), \
-            size=(w * 0.25, h * 0.25), anchor=(0, 0), text="Reculer")
+            size=(w * 0.25, h * 0.25), anchor=(0, 0), text="Reculer", \
+            textKwargs=tk)
         self.add_widget("left_button", Button, (w * 0.25, h * 0.5), \
-            size=(w * 0.25, h * 0.25), anchor=(0, 0), text="Gauche")
+            size=(w * 0.25, h * 0.25), anchor=(0, 0), text="Gauche", \
+            textKwargs=tk)
         self.add_widget("right_button", Button, (w * 0.75, h * 0.5), \
-            size=(w * 0.25, h * 0.25), anchor=(0, 0), text="Droite")
+            size=(w * 0.25, h * 0.25), anchor=(0, 0), text="Droite", \
+            textKwargs=tk)
         self.add_widget("quit_button", Button, (w * 0.5, h * 0.5), \
-            size=(w * 0.2, h * 0.2), anchor=(0, 0), text="Quitter")
+            size=(w * 0.2, h * 0.2), anchor=(0, 0), text="Quitter", \
+            textKwargs=tk)
 
     def update(self):
         get_gui().draw_background_color((255, 255, 255))
